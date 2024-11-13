9b6388dddedd6697ba83c12643335be786aa24ba
alexs <alexs@ALEX>
2024-11-12 20:57:04 -0600

    Creacion de proyecto grupo 04

---
+++ src/main/java/ProyectoGrupo04/grupo04/Grupo04Application.java
@@ -0,0 +1,13 @@
+package ProyectoGrupo04.grupo04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo04Application.class, args);
+	}
+
+}
